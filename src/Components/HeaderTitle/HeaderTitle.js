7e1c8dca3ce8056792f3888106e106a8d1b8d90f
dinaabdullah2 <dinaabdallah21244@gmail.com>
2023-06-09 11:46:17 +0300

    donation

--- src/Components/HeaderTitle/HeaderTitle.js
+++ src/Components/HeaderTitle/HeaderTitle.js
@@ -9,7 +9,7 @@ export default function HeaderTitle(props) {
                 <div className={`${styles.section__body}`}>
                     <h3 className={`${styles.section__title}`}>{props.title}</h3>
                     <p className={`${styles.section__para}`}>  {props.para}</p>
-                    <h4 className={`${styles.section__price}`}>{props.price + " " + t('جنيه') }</h4>
+                    <h4 className={`${styles.section__price}`}>{props.price }</h4>
                 </div>
             </section>
 
